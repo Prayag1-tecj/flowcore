272c9de1ee6f8241b20fe49a143a564986c68771
Prayag1-tecj <prayag.raj.9887@gmail.com>
2026-06-12 10:53:24 +0530

    feat: setup Django backend with PostgreSQL and custom user model

---
+++ backend/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        USER = "USER", "User"
+
+    role = models.CharField(
+        max_length=10,
+        choices=Roles.choices,
+        default=Roles.USER
+    )
+
+    def __str__(self):
+        return self.username
